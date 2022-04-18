5ed12be93b5b64698e7f9e6ff2a6c01965107ce1
Andrew174194 <andrew174194@gmail.com>
2022-04-18 22:06:04 +0300

    Try to add comments

--- app/main.py
+++ app/main.py
@@ -5,9 +5,20 @@ app = Flask(__name__)
 
 app.config["CLIENT_IMAGES"] = '/app/files'
 
-@app.route("/", methods=["GET"])
+@app.route("/", methods=["GET", "POST"])
 def index():
-    return render_template('main.html')
+    message = ""
+    
+    if request.method == 'GET':
+        with open("feedback.txt", "r") as file:
+            message = file.read()
+
+    if request.method == 'POST':
+        message = request.form['feedback'].text()
+        with open("feedback.txt", "w") as file:
+            file.write(message)
+
+    return render_template('main.html', feed_mess=message)
 
 
 @app.route("/static", methods=["GET"])
