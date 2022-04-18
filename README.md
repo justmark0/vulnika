6bc7d120992d78ff05d65aff0c62e09c24bd6da7
Andrew174194 <andrew174194@gmail.com>
2022-04-18 23:38:06 +0300

    Patches & final commit

--- README.md
+++ README.md
@@ -1,5 +1,9 @@
 # vulnika
-Vulnerable web app. RCE
+Vulnerable flask application with examples of exploitation of:
+- Broken Access Control
+- Injection (Bash scripting)
+- Remote Code Execution
+- Cross-site scripting (XSS)
 
 ### Run:
 To run just start docker-compose `docker-compose up`
@@ -7,3 +11,5 @@ To run just start docker-compose `docker-compose up`
 ### Service:
 Service will be available at [http://127.0.0.1:8089](http://0.0.0.0:8089/)
 
+### Patch:
+Change the files from original `app` folder to ones from `patches`. In particular, `main.py` and `templates/main.html` files.
---
+++ patches/app/main.py
@@ -0,0 +1,40 @@
+from flask import *
+import os
+
+app = Flask(__name__)
+
+app.config["CLIENT_IMAGES"] = '/app/files'
+
+@app.route("/", methods=["GET", "POST"])
+def index():
+    message = ""
+    
+    if request.method == 'GET':
+        with open("feedback.txt", "r") as file:
+            message = file.read()
+
+    if request.method == 'POST':
+        message = request.form['feedback']
+        with open("feedback.txt", "w") as file:
+            file.write(message)
+
+    return render_template('main.html', feed_mess=escape(message))
+
+
+@app.route("/static", methods=["GET"])
+def static_files():
+    src = request.args.get("src")
+    return send_from_directory(f'/app/files/{src}', as_attachment=True)
+
+
+@app.route("/feedback", methods=["POST"])
+def feedback():
+    if request.method == 'POST':
+        data = request.get_json()
+        with open("feedback.txt", "w") as file:
+            file.write(data.get('feedback'))
+    return 'ok'
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=9998)
