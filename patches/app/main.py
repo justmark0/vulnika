8b08dda8582748455f4d91af562f3815ed8e2626
Andrew174194 <andrew174194@gmail.com>
2022-04-18 23:58:47 +0300

    send_from_directory fix
    The final final fix :)

--- patches/app/main.py
+++ patches/app/main.py
@@ -24,15 +24,15 @@ def index():
 @app.route("/static", methods=["GET"])
 def static_files():
     src = request.args.get("src")
-    return send_from_directory(f'/app/files/{src}', as_attachment=True)
+    return send_from_directory(app.config["CLIENT_IMAGES"], src)
 
 
 @app.route("/feedback", methods=["POST"])
 def feedback():
     if request.method == 'POST':
-        data = request.get_json()
+        data = request.get_json().get('feedback')
         with open("feedback.txt", "w") as file:
-            file.write(data.get('feedback'))
+            file.write(data)
     return 'ok'
 
 
